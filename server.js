e276fb2fa90bc9be1c439a74eb4065c579410e32
Gustavo Triana <soportedocenteia@gmail.com>
2026-02-17 19:50:54 -0400

    Forzar IPv4 para Nodemailer

--- server.js
+++ server.js
@@ -57,7 +57,8 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    family: 4 // Force IPv4 to avoid ENETUNREACH errors
 });
 
 
