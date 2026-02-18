1876186f1f636376975ff6ffe8621df3f6cb1456
Gustavo Triana <soportedocenteia@gmail.com>
2026-02-17 20:10:24 -0400

    Fix critico de Nodemailer: Forzar IPv4 y puerto 587

--- server.js
+++ server.js
@@ -55,14 +55,16 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
-    secure: false, // OBLIGATORIO false para puerto 587
+    secure: false, // true para 465, false para otros puertos
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
     tls: {
+        ciphers: 'SSLv3', // Ayuda con compatibilidad
         rejectUnauthorized: false
-    }
+    },
+    family: 4 // <--- ESTO ES CRUCIAL: Fuerza IPv4 y evita el error ENETUNREACH
 });
 
 
